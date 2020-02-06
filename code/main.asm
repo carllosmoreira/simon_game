4d1c12e4a53846d860c3c9eddd7640c761dd65f6
brenner <brenner.batista@outlook.com>
2020-02-06 11:31:54 -0300

    Implementação da rotina de inicialização

--- code/main.asm
+++ code/main.asm
@@ -30,9 +30,9 @@ Start:
  	
 ;--- ----
 Main:
-	goto		Rotinainicializacao
+	call		Rotinainicializacao
 	
-	
+;--- ----	
 Rotinainicializacao:
 	
 	bcf		STATUS,RP0		; Muda para o bank2 10
@@ -40,4 +40,42 @@ Rotinainicializacao:
 	movlw		0x0F			; W = B'0000 1111 '
 	movwf		PORTA			; LEDs iniciam ligados		
 	call		delay_1s		; Chama a função de delay
-	clrf		PORTA			; Limpa todos os pinos RA0-RA3	
+	clrf		led_cnt		; led_cnt == 0
+
+
+;--- ----
+LedContLoop:
+
+	clrf		PORTA			; Limpa todos os pinos RA0-RA3
+	movlw		.0
+	subwf		led_cnt, W		
+	btfsc		STATUS, Z		; led_cnt =0 ?
+	bsf		PORTA, RA0		; Sim, led_cnt ==0 ; Acende LED0-RA0
+	
+	movlw		.1
+	subwf		led_cnt, W		
+	btfsc		STATUS, Z		; led_cnt =1 ?
+	bsf		PORTA, RA1		; Sim, led_cnt ==1 ; Acende LED2-RA1
+	
+	movlw		.2
+	subwf		led_cnt, W		
+	btfsc		STATUS, Z		; led_cnt =2 ?
+	bsf		PORTA, RA2		; Sim, led_cnt ==2 ; Acende LED2-RA2
+	
+	movlw		.3
+	subwf		led_cnt, W		
+	btfsc		STATUS, Z		; led_cnt =3 ?
+	bsf		PORTA, RA3		; Sim, led_cnt ==3 ; Acende LED3-RA3
+	
+	call		Delay_200ms
+	incf		led_cnt, F		; Incrementa led_cnt
+	
+	movlw		.4
+	subwf		led_cnt, W		
+	btfss		STATUS, Z		; led_cnt = 4 ?
+	goto		LedContLoop:	; Não, volta ao inicio da rotina
+	clrf		PORTA			; Sim, apaga todos os LEDs
+	return
+	
+	
+Delay	
